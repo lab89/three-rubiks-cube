2416a94e5ba5fdea9a70ef007915ca2397c711f5
hong <ghhong@wemb.co.kr>
2020-08-07 02:03:07 +0900

    change corner rotation for sticker
    change corner rotation for sticker

--- js/THREERubiksCube.js
+++ js/THREERubiksCube.js
@@ -42,11 +42,79 @@ if (typeof Object.assign != 'function') {
  *  }
 * */
 THREE.RubiksCube = function RubiksCube(){
-    THREE.Group.apply(this);
+	THREE.Group.apply(this);
+	this._corners = [];
 };
 THREE.RubiksCube.prototype = Object.create(THREE.Group.prototype);
 THREE.RubiksCube.prototype.constructor = THREE.RubiksCube;
-THREE.RubiksCube.prototype.createBlock =  function createBlock(options, orientation){
+THREE.RubiksCube.prototype.createConerBlock = function createCornerBlock(options, orientation){	
+
+	const commonStyle = {
+		position: "absolute",
+		// backgroundColor: "black",
+		borderRadius : "30px",
+		width: options.size.width + "px",
+		border : "1px dashed black",
+		height: options.size.height + "px",
+	};
+	
+	const blockElement = document.createElement("div");
+	blockElement.style.position = "absolute";
+	blockElement.style.display = "block";
+	blockElement.style.transformStyle = "preserve-3d";
+	blockElement.top = (options.size.width * -1) + "px";
+	
+	
+	const faceElement = document.createElement("div");
+	Object.assign(faceElement.style, commonStyle);
+	const sticker = faceElement.cloneNode(false);
+	sticker.style.backgroundColor = "cyan";
+	sticker.style.borderBottomRightRadius = "150px";
+	faceElement.appendChild(sticker);
+	
+	/** top - down face **/
+	const top = faceElement.cloneNode(true);
+	top.style.transform = "translateX("+ (-options.size.width / 2) + "px)" + "translateY(" + (-options.size.height) + "px)" + "rotate3d(1, 0, 0, -90deg) ";
+	// top.style.backgroundColor = "green";
+	blockElement.appendChild(top);
+	
+	const down = faceElement.cloneNode(true);
+	down.style.transform = "translateX("+ (-options.size.width / 2) + "px)" + "rotate3d(1, 0, 0, -90deg) ";
+	// down.style.backgroundColor = "red";
+	blockElement.appendChild(down);
+	
+	/** left - right face **/
+	const left = faceElement.cloneNode(true);
+	left.style.transform = "translateX(" +  (options.size.width * -1) + "px)"+ "translateY(" + (-options.size.height/ 2) + "px)" + "rotate3d(0, 1, 0, 90deg)";
+	// left.style.backgroundColor = "cyan";
+	blockElement.appendChild(left);
+	
+	const right = faceElement.cloneNode(true);
+	right.style.transform =" rotate3d(0, 1, 0, 90deg)"+ "translateY(" + (-options.size.height / 2) + "px)";
+	// right.style.backgroundColor = "blue";
+	blockElement.appendChild(right);
+	
+	
+	/** front - back face **/
+	const front =  faceElement.cloneNode(true);
+	front.style.transform = "translateX("+ (-options.size.width / 2) + "px)" +"translateZ(" +  (options.size.depth  / 2) + "px)" + "translateY(" + (-options.size.height / 2) + "px)" + "rotate3d(0, 1, 0, 0deg)";
+	// front.style.backgroundColor = "black";
+	blockElement.appendChild(front);
+	
+	const back =  faceElement.cloneNode(true);
+	back.style.transform = "translateX("+ (-options.size.width / 2) + "px)"  + "translateZ(" +  (options.size.depth  * -1 / 2) + "px)" + "translateY(" + (-options.size.height / 2) + "px)" +  "rotate3d(0, 1, 0, -180deg)";
+	// back.style.backgroundColor = "gray";
+	blockElement.appendChild(back);
+	
+	const block = new THREE.CSS3DObject(blockElement);	
+	Object.assign(block.userData, {"orientation": orientation.clone()});		
+	
+	block.rotateOnAxis(new THREE.Vector3(0, 1, 0), this._corners.length * 90 * Math.PI / 180 * -1)
+	this._corners.push(block);
+	
+	return block;
+}
+THREE.RubiksCube.prototype.createBlock =  function createBlock(options, orientation){	
 	const commonStyle = {
 		position: "absolute",
 		// backgroundColor: "black",
@@ -116,19 +184,32 @@ THREE.RubiksCube.prototype.createBlock =  function createBlock(options, orientat
 
 THREE.GANCube333 = function(options){
 	THREE.RubiksCube.apply(this, [document.createElement("div")]);
-	[-1, 0, 1].forEach((y)=>{
-	    [-1, 0, 1].forEach((x)=>{
-	        [-1, 0, 1].forEach((z)=>{
-		        const block = this.createBlock(options, new THREE.Vector3(x, y , z));
-		        block.position.x = x * options.size.width;
-		        block.position.y = y * options.size.height;
-		        block.position.z = z * options.size.depth;
-		        block.userData.orientation = new THREE.Vector3(x, y, z);
-		        // block.rotateOnAxis (new THREE.Vector3(1, 0, 0), 45 * Math.PI / 180);
-		        this.add(block);
-	        })
-	    })
-	});
+	
+	//corner 
+	[-1, 1].forEach(function(y){
+		[-1, 1].forEach(function(x){			
+			if(x === -1){
+				[1, -1].forEach(function(z){
+					const corner = this.createConerBlock(options, new THREE.Vector3(x, y, z))
+					this.add(corner)
+					corner.position.x = x * options.size.width;
+					corner.position.y = -y * options.size.height;
+					corner.position.z = z * options.size.depth;
+					if(y === 1) corner.rotateOnAxis(new THREE.Vector3(0, 0, 1), 90 * Math.PI / 180)
+				}.bind(this))
+			}else if(x === 1){
+				[-1, 1].forEach(function(z){
+					const corner = this.createConerBlock(options, new THREE.Vector3(x, y, z))
+					this.add(corner)
+					corner.position.x = x * options.size.width;
+					corner.position.y = -y * options.size.height;
+					corner.position.z = z * options.size.depth;
+					if(y === 1) corner.rotateOnAxis(new THREE.Vector3(0, 0, 1), 90 * Math.PI / 180)
+				}.bind(this))
+			}
+			
+		}.bind(this))
+	}.bind(this))
 };
 THREE.GANCube333.prototype = Object.create(THREE.RubiksCube.prototype);
 THREE.GANCube333.prototype.constructor = THREE.GANCube333;
