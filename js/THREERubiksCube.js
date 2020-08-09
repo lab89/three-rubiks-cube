22d3fe700e6484b53abf055c09725dc1d05780b1
hong <ghhong@wemb.co.kr>
2020-08-09 13:32:06 +0900

    attach sticker
    attach sticker

--- js/THREERubiksCube.js
+++ js/THREERubiksCube.js
@@ -28,30 +28,19 @@ if (typeof Object.assign != 'function') {
 		configurable: true
 	});
 }
-
 // THREE exist check
 // THREE CSS3DObject exist check
 
-/**
-* param
- *  blockColor : "",
- *  size : {
- *    width :
- *    height :
- *    depth :
- *  }
-* */
-THREE.RubiksCube = function RubiksCube(){
+THREE.NNNCube = function MagicCube(){
 	THREE.Group.apply(this);		
 };
-THREE.RubiksCube.prototype = Object.create(THREE.Group.prototype);
-THREE.RubiksCube.prototype.constructor = THREE.RubiksCube; 
-
+THREE.NNNCube.prototype = Object.create(THREE.Group.prototype);
+THREE.NNNCube.prototype.constructor = THREE.NNNCube; 
 
 THREE.Cube333 = function Cube333(options){
-	THREE.RubiksCube.apply(this);
-	this._blocks = [];
-	const coordInfo = {
+	THREE.NNNCube.apply(this);
+	this._options = options;	
+	this.coordInfo = {
 		x : 0,
 		l : -1,
 		r : 1,
@@ -59,38 +48,40 @@ THREE.Cube333 = function Cube333(options){
 		b : -1,
 		u : -1,
 		d : 1,
-		m : 0
-	};	
-	
-	const blocks = [
-		"luf", "ruf", "rub", "lub", 
-		"ldf", "rdf", "rdb", "ldb",
-		"lmf", "rmf", "rmb", "lmb", 
-		"xuf", "rux", "xub", "lux"
-		,"xdf", "rdx", "xdb", "ldx"
-		,"mmf", "rmm", "lmm", "mmb", "mum", "mdm"
+	};		
+
+	this.blocks = [
+		["luf", "ruf", "rub", "lub"],
+		["ldf", "rdf", "rdb", "ldb"],
+		["lxf", "rxf", "rxb", "lxb"],		
+		["xdf", "rdx", "xdb", "ldx"],
+		["xuf", "rux", "xub", "lux"],
+		["xxf", "rxx", "xxb", "lxx"],		
+		["xux"],
+		["xdx"]
 	]
-	blocks.forEach(function(coord){
-		const coords = coord.split("");
-		const coordVector = new THREE.Vector3(coordInfo[coord[0]], coordInfo[coord[1]], coordInfo[coord[2]])				
-		const block = this.createBlock(options, coordVector)
-		this.add(block);
-		block.userData.cublet = coord;
-		block.position.x = coordVector.x * options.size.width;
-		block.position.y = -coordVector.y * options.size.height;
-		block.position.z = coordVector.z * options.size.depth;	
-		if(coords.includes("d")) block.rotateOnAxis(new THREE.Vector3(0, 0, 1), 90 * Math.PI / 180);	
+	this.blocks.forEach(function(coordsArr){
+		coordsArr.forEach(function(coordString){
+			const coords = coordString.split("");
+			const coordVector = new THREE.Vector3(this.coordInfo[coordString[0]], this.coordInfo[coordString[1]], this.coordInfo[coordString[2]])				
+			const block = this.createBlock(options, coordVector)
+			this.add(block);
+			block.name = coordString; //init coord String
+			block.position.x = coordVector.x * options.size.width;
+			block.position.y = -coordVector.y * options.size.height;
+			block.position.z = coordVector.z * options.size.depth;				
+		}.bind(this))		
 	}.bind(this)) 
 }
-THREE.Cube333.prototype = Object.create(THREE.RubiksCube.prototype);
+THREE.Cube333.prototype = Object.create(THREE.NNNCube.prototype);
 THREE.Cube333.prototype.constructor = THREE.Cube333;
 THREE.Cube333.prototype.createBlock = function createBlock(options, orientation){
 	const commonStyle = {
 		position: "absolute",
-		// backgroundColor: "black",
+		backgroundColor: "black",
 		borderRadius : "30px",
+		backfaceVisibility : "visible",
 		width: options.size.width + "px",
-		border : "1px dashed black",
 		height: options.size.height + "px",
 	};
 	
@@ -101,86 +92,137 @@ THREE.Cube333.prototype.createBlock = function createBlock(options, orientation)
 	
 	
 	const faceElement = document.createElement("div");
-	Object.assign(faceElement.style, commonStyle);
-	// const sticker = faceElement.cloneNode(false);
-	// sticker.style.backgroundColor = "cyan";
-	// sticker.style.borderBottomRightRadius = "150px";
-	// faceElement.appendChild(sticker);
+	Object.assign(faceElement.style, commonStyle);	
 	
 	/** top - down face **/
 	const top = faceElement.cloneNode(true);
+	top.className = "u";
 	top.style.transform = "translateX("+ (-options.size.width / 2) + "px)" + "translateY(" + (-options.size.height) + "px)" + "rotate3d(1, 0, 0, -90deg) ";
-	// top.style.backgroundColor = "green";
 	blockElement.appendChild(top);
 	
 	const down = faceElement.cloneNode(true);
-	down.style.transform = "translateX("+ (-options.size.width / 2) + "px)" + "rotate3d(1, 0, 0, -90deg) ";
-	// down.style.backgroundColor = "red";
+	down.className = "d";
+	down.style.transform = "translateX("+ (-options.size.width / 2) + "px)" + "rotate3d(1, 0, 0, 90deg) ";
 	blockElement.appendChild(down);
 	
 	/** left - right face **/
 	const left = faceElement.cloneNode(true);
-	left.style.transform = "translateX(" +  (options.size.width * -1) + "px)"+ "translateY(" + (-options.size.height/ 2) + "px)" + "rotate3d(0, 1, 0, 90deg)";
-	// left.style.backgroundColor = "cyan";
+	left.className = "l";
+	left.style.transform = "translateX(" +  (options.size.width * -1) + "px)"+ "translateY(" + (-options.size.height/ 2) + "px)" + "rotate3d(0, 1, 0, 90deg)";	
 	blockElement.appendChild(left);
 	
 	const right = faceElement.cloneNode(true);
-	right.style.transform =" rotate3d(0, 1, 0, 90deg)"+ "translateY(" + (-options.size.height / 2) + "px)";
-	// right.style.backgroundColor = "blue";
-	blockElement.appendChild(right);
-	
+	right.className = "r";
+	right.style.transform =" rotate3d(0, 1, 0, 90deg)"+ "translateY(" + (-options.size.height / 2) + "px)";	
+	blockElement.appendChild(right);	
 	
 	/** front - back face **/
 	const front =  faceElement.cloneNode(true);
-	front.style.transform = "translateX("+ (-options.size.width / 2) + "px)" +"translateZ(" +  (options.size.depth  / 2) + "px)" + "translateY(" + (-options.size.height / 2) + "px)" + "rotate3d(0, 1, 0, 0deg)";
-	// front.style.backgroundColor = "black";
+	front.className = "f";
+	front.style.transform = "translateX("+ (-options.size.width / 2) + "px)" +"translateZ(" +  (options.size.depth  / 2) + "px)" + "translateY(" + (-options.size.height / 2) + "px)" + "rotate3d(0, 1, 0, 0deg)";	
 	blockElement.appendChild(front);
 	
 	const back =  faceElement.cloneNode(true);
-	back.style.transform = "translateX("+ (-options.size.width / 2) + "px)"  + "translateZ(" +  (options.size.depth  * -1 / 2) + "px)" + "translateY(" + (-options.size.height / 2) + "px)" +  "rotate3d(0, 1, 0, -180deg)";
-	// back.style.backgroundColor = "gray";
+	back.className = "b"
+	back.style.transform = "translateX("+ (-options.size.width / 2) + "px)"  + "translateZ(" +  (options.size.depth  * -1 / 2) + "px)" + "translateY(" + (-options.size.height / 2) + "px)" +  "rotate3d(0, 1, 0, -180deg)";	
 	blockElement.appendChild(back);
+
+	const xplane =  faceElement.cloneNode(true);
+	xplane.className = "z";
+	xplane.style.borderRadius = "0px"
+	xplane.style.transform = "translateX("+ (-options.size.width / 2) + "px)" + "translateY(" + (-options.size.height / 2) + "px)" + "rotate3d(1, 0, 0, -90deg) ";
+	blockElement.appendChild(xplane);
+
+	// see through block plane
+	const yplane =  faceElement.cloneNode(true);
+	yplane.className = "y";
+	yplane.style.borderRadius = "0px"
+	yplane.style.transform = "translateX("+ (-options.size.width / 2) + "px)" + "translateY(" + (-options.size.height / 2) + "px)";
+	blockElement.appendChild(yplane);
+
+	const zplane =  faceElement.cloneNode(true);
+	zplane.className = "x";
+	zplane.style.borderRadius = "0px"
+	zplane.style.transform = "translateX("+ (-options.size.width / 2) + "px)" + "translateY(" + (-options.size.height / 2) + "px)" + "rotate3d(0, 1, 0, 90deg) ";
+	blockElement.appendChild(zplane);
 	
 	const block = new THREE.CSS3DObject(blockElement);
 	Object.assign(block.userData, {"orientation": orientation.clone()});
 	
-	block.rotateOnAxis(new THREE.Vector3(0, 1, 0), this._blocks.length * 90 * Math.PI / 180)
-	this._blocks.push(block);
+	block.rotateOnAxis(new THREE.Vector3(0, 1, 0), this.children.length * 90 * Math.PI / 180)	
 
 	return block;
 }
 
+
+/**
+* param
+ *  blockColor : "black, white etc..",
+ *  size : {
+ *    width :
+ *    height :
+ *    depth :
+ *  },
+ *  fitment : ,
+ *  stickerColorset : ,
+ *  mirror : true / false
+* */
 THREE.GANCube333 = function(options){
 	THREE.Cube333.apply(this, [options]);
+	this.stickers = {
+		"luf" : ["borderBottomRightRadius"],
+		"ldf" : ["borderTopRightRadius"],
+		"lxf" : ["borderTopRightRadius", "borderBottomRightRadius"],
+		"xdf" : ["borderTopRightRadius", "borderTopLeftRadius"],
+		"xuf" : ["borderBottomRightRadius", "borderBottomLeftRadius"]
+	}
+	this.blocks.forEach(function(arr){
+		arr.forEach(function(coord, i){
+			this.attachSticker(coord, arr[0], i);
+		}.bind(this))
+	}.bind(this))
 };
 THREE.GANCube333.prototype = Object.create(THREE.Cube333.prototype);
 THREE.GANCube333.prototype.constructor = THREE.GANCube333;
-THREE.GANCube333.prototype.attachSticker = function attachSticker(options){
-	/**
-	 * option : {
-	 * 	stickerColoerSet : f b l r u b .. 
-	 * }
-	 */
-	//standard : left - top - front corner (bottomRightRadius)
-	/**
-	 * ORDER : ->(90 rotate, y axis)
-	 * "luf"(standard : bottomRightRadius), "ruf", "rub", "lub", 
-		"ldf", "rdf", "rdb", "ldb",
-		"lmf", "rmf", "rmb", "lmb", 
-		"xuf", "rux", "xub", "lux"
-		,"xdf", "rdx", "xdb", "ldx"
-		,"mmf", "rmm", "lmm", "mmb", "mum", "mdm"
-	 */
-	const corner = {
-		backgroundColor : "",
-		borderBottomRightRadius : ""
+
+THREE.GANCube333.prototype.attachSticker = function attachSticker(realCoord, stickerCoord, idx){	
+	function faceRotate(text, i){
+		const arr = ["f", "r", "b", "l"];
+		const index = arr.indexOf(text);
+		if(index >= 0){
+			const indexSum = index + i;
+			return arr[indexSum % arr.length];
+		}	
+		else return text;
+	}
+	const block = this.getObjectByName(realCoord); //real coord	
+	const blockCoord = block.name.split("");	
+	const sticker_coords = stickerCoord.split(""); //sticker coord -> must transform! by faceRotate function
+	const sum = Math.abs(this.coordInfo[blockCoord[0]]) + Math.abs(this.coordInfo[blockCoord[1]]) + Math.abs(this.coordInfo[blockCoord[2]]);
+	const style = {
+		width : this._options.fitment === "fully_fitted" ? "97%" : "90%",
+		height : this._options.fitment === "fully_fitted" ? "97%" : "90%",
+		margin : this._options.fitment === "fully_fitted" ? "2px" : "8px",
+		borderRadius : "30px"
 	};
-	const edge = {
-		backgroundColor : "",
-		borderBottomRadius : ""
-	};
-	const center = {
-		backgroundColor : "",
-		borderRadius : ""
-	};
+	const element = block.element;
+	sticker_coords.forEach(function(sc){			
+		const face = element.getElementsByClassName(sc);			
+		if(face.length){
+			Object.assign(style, {
+				backgroundColor : this._options.stickerColorSet[faceRotate(sc, idx % 4)],				
+			})			
+			if(this.stickers[stickerCoord]){
+				this.stickers[stickerCoord].forEach(function(radius){
+					style[radius] = "80px";
+				}.bind(this))
+			}else{
+				style["borderRadius"] = "50% 50% 50% 50%";
+			}	
+
+			const sticker = document.createElement("div");
+			Object.assign(sticker.style, style)
+			face[0].appendChild(sticker);
+		}				
+	}.bind(this))
 }
